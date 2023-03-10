214f7ea2308d59d55b9f593a126247c4f91e8c4a
SachinthaMadhushanka <17100@eng.pdn.ac.lk>
2023-03-10 12:29:41 +0530

    Add Image Uploader Service

---
+++ client-app/src/components/ImageUploader.js
@@ -0,0 +1,105 @@
+import {useState} from "react"
+import {FirebaseService} from "../services-common/firebase-service";
+
+function ImageUploader() {
+    const [file, setFile] = useState();
+    const [onUploading, setOnUploading] = useState(false);
+    const [deleteURL, setDeleteURL] = useState("");
+    const [status, setStatus] = useState("");
+    const [multipleDeleteURL, setMultipleDeleteUrl] = useState("");
+    const [multipleURLs, setMultipleURLs] = useState([]);
+
+
+    // Handles input change event and updates state
+    function handleChange(event) {
+        console.log(event.target.files);
+        setFile(event.target.files);
+    }
+
+    const onClickUpload = () => {
+        setStatus("Uploading");
+        FirebaseService.uploadFiles("1", file).then((url) => {
+            console.log(url);
+            setStatus("Upload success");
+        }).catch(err => {
+            console.log(err);
+            setStatus("Upload failed");
+        })
+    }
+
+    const onClickDeleteImage = () => {
+        FirebaseService.deleteFile(deleteURL).then((url) => {
+            setStatus("Image Deleted")
+        }).catch(err => {
+            setStatus("Image Delete Error");
+            console.log(err);
+        })
+    }
+
+    function onChangeDeleteURL(event) {
+        setDeleteURL(event.target.value);
+    }
+
+    function onChangeMultipleDeleteURL(event) {
+        setMultipleDeleteUrl(event.target.value);
+    }
+
+    const onClickAddURL = () => {
+        setMultipleURLs(prevState => {
+            return [...prevState, multipleDeleteURL]
+        })
+
+        setStatus("File URL added.");
+
+        setMultipleDeleteUrl("");
+    }
+
+    const deleteMultipleFiles = () => {
+      FirebaseService.deleteFiles(multipleURLs).then(result => {
+          setStatus("deleted multiple files.");
+      }).catch(err => {
+          setStatus("error deleting multiple files");
+          console.log(err);
+      })
+    }
+
+
+    return (
+        <div>
+            <br/>
+            <br/>
+            <input type="file" accept="image/*" multiple onChange={handleChange}/>
+            <button onClick={onClickUpload}>Upload to Firebase</button>
+            <br/>
+            <br/>
+
+            <p style={{color: "red"}}>{status}</p>
+
+            <br/>
+            <br/>
+            <input type="text" value={deleteURL} onChange={onChangeDeleteURL}/>
+            <button onClick={onClickDeleteImage}>Delete File</button>
+
+            <br/>
+            <br/>
+            <input type="text" value={multipleDeleteURL} onChange={onChangeMultipleDeleteURL}/>
+            <button onClick={onClickAddURL}>Add URL</button>
+
+            <br/>
+
+            {multipleURLs.map(url => {
+                return <li>{url}</li>
+            })}
+
+            <br/>
+            <br/>
+            <button onClick={deleteMultipleFiles}>Delete All Files</button>
+
+
+
+
+        </div>
+    );
+}
+
+export default ImageUploader;
