99d805d7799b56553c22d06a1a55c96c0b897cab
Kasun Kavinda <kasunkavinda.bc@gmail.com>
2023-03-21 16:26:29 +0530

    phase 3 (#19)

--- client-app/src/components/NavBar/NavBar.js
+++ client-app/src/components/NavBar/NavBar.js
@@ -1,33 +1,58 @@
 import { Button, Navbar, NavbarBrand, NavbarText } from "reactstrap";
 import { useNavigate } from "react-router-dom";
 
-import "./styles.scss"
+import "./styles.scss";
 
 function NavBar(props) {
-    const navigate = useNavigate();
-    return (
-        <Navbar className="cus_navbar" dark>
-            <NavbarBrand href="/" style={{ marginLeft: "40px" }}>
-                <img
-                    alt="logo"
-                    src="/Assets/Images/logo.png"
-                    style={{
-                        height: 70,
-                        width: 260
-                    }}
-                />
-            </NavbarBrand>
+  const navigate = useNavigate();
 
+  const isCustomer = localStorage.getItem("customer");
+  return (
+    <Navbar className="cus_navbar" dark>
+      <NavbarBrand href="/" style={{ marginLeft: "40px" }}>
+        <img
+          alt="logo"
+          src="/Assets/Images/logo.png"
+          style={{
+            height: 70,
+            width: 260,
+          }}
+        />
+      </NavbarBrand>
 
-            <NavbarText className="explore_txt white-text" >Explore</NavbarText>
-            <NavbarText className="vacation_txt white-text" >Vacation Rental</NavbarText>
-            <NavbarText className="community_txt white-text" >Community</NavbarText>
-            <Button outline className="primaryButton smallMarginLeftRight" onClick={props.goToListProperty}>List Your Property</Button>
-            <Button outline className="primaryButton smallMarginLeftRight" onClick={(() => navigate('/reservations'))}>My Reservations</Button>
+      {isCustomer === "true" ? (
+        <>
+          <NavbarText className="explore_txt white-text">Explore</NavbarText>
+          <NavbarText className="vacation_txt white-text">
+            Vacation Rental
+          </NavbarText>
+          <NavbarText className="community_txt white-text">
+            Community
+          </NavbarText>{" "}
+        </>
+      ) : (
+        <>
+          <NavbarText className="help_button">Help</NavbarText>
+          <NavbarText className="faq-text">Faq</NavbarText>
+        </>
+      )}
 
-        </Navbar>
-    );
-
+      <Button
+        outline
+        className="primaryButton smallMarginLeftRight"
+        onClick={() => navigate("/list-property")}
+      >
+        List Your Property
+      </Button>
+      <Button
+        outline
+        className="primaryButton smallMarginLeftRight"
+        onClick={() => navigate("/reservations")}
+      >
+        My Reservations
+      </Button>
+    </Navbar>
+  );
 }
 
 export default NavBar;
